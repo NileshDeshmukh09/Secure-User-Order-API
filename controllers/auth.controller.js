e8503e4a109ee6bc068b5659d3d05f116f2afc89
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2023-02-08 03:41:51 +0530

    added Codes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,25 +5,21 @@ const jwt = require("jsonwebtoken");
 
 exports.signup = async (req, res) => {
 
-    const salt =  bcrypt.genSaltSync(10);
-    const hashedpassword = await req.body.password;
-    
-    
     const UserDetailsStoredInDB = {
         name: req.body.name,
-        userID : req.body.userID,
+        userID: req.body.userID,
         phoneNumber: req.body.phoneNumber,
         // Hash the password
         password: bcrypt.hashSync(req.body.password, 8),
     }
-    
+
     /**
      * Create the New User and Added to the database
     */
-   try {
-       const newUser = await User.create(UserDetailsStoredInDB);
-       newUser.save();
-      
+    try {
+        const newUser = await User.create(UserDetailsStoredInDB);
+        newUser.save();
+
         /**
         *  response
         */
@@ -36,7 +32,7 @@ exports.signup = async (req, res) => {
         }
 
         console.log(ResponseOfNewUser);
-         return res.status(201).send({
+        return res.status(201).send({
             success: true,
             message: `${newUser.userID} , Added Successully !`,
             user: ResponseOfNewUser
@@ -45,6 +41,7 @@ exports.signup = async (req, res) => {
 
         console.log(err.message);
         return res.status(500).send({
+            success: false,
             message: "Internal Server Error ,when Insert User !"
         })
     }
@@ -60,39 +57,45 @@ exports.signin = async (req, res) => {
 
     //Search the user if it exists 
     try {
-        var user = await User.findOne({ phoneNumber : req.body.phoneNumber });
+        var user = await User.findOne({ phoneNumber: req.body.phoneNumber });
+
+
+
+        if (user == null) {
+            return res.status(400).send("phoneNumber Doesn't Exist !")
+        }
+
+        //User is exists , check for the valid password
+        const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).send("Invalid Password")
+        }
+
+        //** Successfull login */
+        //I need to generate access token now
+        console.log("userID :", user.userID);
+        const token = jwt.sign({ id: user.userID }, process.env.SECRET, {
+            expiresIn: '2h'
+        });
+
+        //Send the response back 
+        return res.status(200).send({
+            status: 200,
+            message: `${user.name} login Successfully !`,
+            user: {
+                name: user.name,
+                userID: user.userID,
+                phoneNumber: user.phoneNumber,
+                accessToken: token
+            }
+        })
     } catch (err) {
         console.log(err.message);
+        return res.status(500).send({
+            success: false,
+            message: "Internal Server Error ,when Insert User !"
+        })
     }
-    
-    if (user == null) {
-        return res.status(400).send("phoneNumber Doesn't Exist !")
-    }
-
-    //User is exists , check for the valid password
-    const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
-
-    if (!isPasswordValid) {
-        return res.status(401).send("Invalid Password")
-    }
-
-    //** Successfull login */
-    //I need to generate access token now
-    console.log( "userID :", user.userID );
-    const token = jwt.sign({ id : user.userID }, process.env.SECRET, {
-        expiresIn: '2h'
-    });
-
-    //Send the response back 
-     return res.status(200).send({
-        status : 200,
-        message: `${user.name} login Successfully !`,
-        user: {
-            name: user.name,
-            userID : user.userID,
-            phoneNumber: user.phoneNumber,
-            accessToken: token
-        }
-    })
 
 };
