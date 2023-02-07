1d6e5b687403dd6c818b41937d1170d8b686f156
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2023-02-07 07:59:11 +0530

    user schema

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+
+/**
+* Schema for the user Model will be provided Here
+*/
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+});
+
+/* These will automatically generates the created and updated fields */
+userSchema.set('timestamps' , true);
+
+module.exports = mongoose.model("User", userSchema);
+
+
