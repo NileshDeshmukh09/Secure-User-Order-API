07dbb3618cf52942f39acaa1c728f8f2953acbfd
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2023-02-07 21:34:10 +0530

    vercel.json

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -6,10 +6,10 @@ exports.addOrder = async (req, res) => {
 
     /** User ID of reported must be present in x-access-token */
     const user = await User.findOne({
-        userID : req.userID
+        userID: req.userID
     });
 
-    console.log("getUser :" , user );
+    console.log("getUser :", user);
 
     const orderObj = {
         sub_total: req.body.sub_total,
@@ -18,39 +18,39 @@ exports.addOrder = async (req, res) => {
     }
 
     try {
-        const newOrder = await Order.create( orderObj );
+        const newOrder = await Order.create(orderObj);
 
         return res.status(201).send({
             message: "Order created Successfully !",
             ticket: newOrder
         })
 
-    }catch( error ){
+    } catch (error) {
 
-        console.log( error );
+        console.log(error);
         return res.status(400).send({
             message: error.message
         })
 
     }
-    
+
 }
 
 
-exports.getOrderByUserID = async ( req, res ) => {
+exports.getOrderByUserID = async (req, res) => {
     /**
  * Controller to fetch the Order based on ID's 
  */
 
 
     const order = await Order.find({
-         userID: req.params.userID
+        userID: req.params.userID
     });
 
     res.status(200).send({
-         status : 200,
-         message : "Order get successfully !",
-         orderDetail : order
+        status: 200,
+        message: "Order get successfully !",
+        orderDetail: order
     });
 }
 
